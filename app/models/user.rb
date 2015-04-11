063b5cb4e0f77a2b0994c195c287e493aa8a60dd
askl56 <amscott@openmailbox.org>
2015-04-11 06:02:49 +0100

    Starting off

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
